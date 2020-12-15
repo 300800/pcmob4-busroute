a7acf555c26a998d4c50cd44b734a7fafbd3b08d
grace <gracechongbl@gmail.com>
2020-12-15 09:51:29 +0800

    InactivtyApplicator

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState, setLoading } from "react";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
+
+const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=83139";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [arrival, setArrival] = useState("");
+
+  function loadBusStopData() {
+    setLoading(true);
+
+    fetch(BUSSTOP_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        //console.log(responseData)
+        const myBus = responseData.services.filter(
+          (item) => item.no === "155"
+        )[0];
+        //console.log(myBus);
+        setArrival(myBus.next.duration_ms);
+        setLoading(false);
+      });
+  }
+
+  useEffect(() => {
+    loadBusStopData();
+    const interval = setInterval(loadBusStopData, 15000);
+    loadBusStopData();
+    //return the function to run when unmounting
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Bus arrival time</Text>
+      <Text style={styles.arrivalTime}>
+        {loading ? <ActivityIndicator size="large" color="#cc8" /> : arrival}
+      </Text>
+      <TouchableOpacity onPress={null} style={styles.button}>
+        <Text style={styles.buttonText}>Refresh</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +53,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 24,
+  },
+  arrivalTime: {
+    fontSize: 36,
+    marginBottom: 24,
+  },
+  button: {
+    backgroundColor: "green",
+    padding: 24,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
   },
 });

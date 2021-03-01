c1a5f5a1fc3382dc2376b1e7921f59de08e7d34a
grace <gracechongbl@gmail.com>
2021-03-01 12:22:14 +0800

    updated duration code by commenting out the strings

--- App.js
+++ App.js
@@ -7,7 +7,10 @@ import {
   TouchableOpacity,
 } from "react-native";
 
-const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=83139";
+const BUSSTOP_NUMBER = 83139;
+const BUS_NUMBER = "155";
+const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=" + BUSSTOP_NUMBER;
+const LOADING_INTERVAL = 60000;
 
 export default function App() {
   const [loading, setLoading] = useState(true);
@@ -19,20 +22,30 @@ export default function App() {
     fetch(BUSSTOP_URL)
       .then((response) => response.json())
       .then((responseData) => {
-        //console.log(responseData)
+        // console.log(responseData);
+
         const myBus = responseData.services.filter(
-          (item) => item.no === "155"
+          (item) => item.no === BUS_NUMBER
         )[0];
-        //console.log(myBus);
-        setArrival(myBus.next.duration_ms);
+        // console.log(myBus);
+
+        // check that duration_s has been extracted correctly
+        const duration_s = Math.floor(myBus.next.duration_ms / 1000);
+        //const duration_s = Math.floor(myBus["next.duration_ms"] / 1000); // same as myBus.next.duration_ms
+        console.log(duration_s);
+
+        // Check minutes and seconds, if they are in number format
+        const minutes = Math.floor(duration_s / 60);
+        const seconds = duration_s % 60;
+
+        setArrival(`${minutes} minutes and ${seconds} seconds`);
         setLoading(false);
       });
   }
 
   useEffect(() => {
-    loadBusStopData();
-    const interval = setInterval(loadBusStopData, 15000);
-    loadBusStopData();
+    const interval = setInterval(loadBusStopData, LOADING_INTERVAL);
+    loadBusStopData(); //need to call it once at the start
     //return the function to run when unmounting
     return () => clearInterval(interval);
   }, []);
@@ -43,7 +56,7 @@ export default function App() {
       <Text style={styles.arrivalTime}>
         {loading ? <ActivityIndicator size="large" color="#cc8" /> : arrival}
       </Text>
-      <TouchableOpacity onPress={null} style={styles.button}>
+      <TouchableOpacity onPress={loadBusStopData} style={styles.button}>
         <Text style={styles.buttonText}>Refresh</Text>
       </TouchableOpacity>
     </View>
